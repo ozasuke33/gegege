90529821ee199d52fa66cce66188259495324c84
ozasuke33 <ozasuke33@gmail.com>
2025-01-08 20:02:14 +0900

    add SDL

--- include/gegege/vulkan_engine/vulkan_engine.hpp
+++ include/gegege/vulkan_engine/vulkan_engine.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+
 namespace gegege {
 
 class VulkanEngine {
+    SDL_Window* sdlWindow;
+
 public:
     void startup();
     void shutdown();
--- src/gegege/vulkan_engine/vulkan_engine.cpp
+++ src/gegege/vulkan_engine/vulkan_engine.cpp
@@ -4,10 +4,25 @@ namespace gegege {
 
 void VulkanEngine::startup()
 {
+    SDL_Log("Vulkan Engine: initialize SDL");
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("Vulkan Engine: Couldn't initialize SDL: %s", SDL_GetError());
+        abort();
+    }
+
+    SDL_Log("Vulkan Engine: create window");
+    sdlWindow = SDL_CreateWindow("gegege::VulkanEngine", 640, 480, SDL_WINDOW_VULKAN);
+    if (!sdlWindow)
+    {
+        SDL_Log("Vulkan Engine: Couldn't create window: %s", SDL_GetError());
+    }
 }
 
 void VulkanEngine::shutdown()
 {
+    SDL_Log("Vulkan Engine: finalize SDL");
+    SDL_Quit();
 }
 
 } // namespace gegege
