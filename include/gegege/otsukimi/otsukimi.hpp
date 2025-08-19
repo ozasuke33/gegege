83da05986cde3e9405d3f2f93f1742fc4b599052
ozasuke33 <ozasuke33@gmail.com>
2025-08-19 20:25:07 +0900

    ウィンドウ生成

--- include/gegege/otsukimi/otsukimi.hpp
+++ include/gegege/otsukimi/otsukimi.hpp
@@ -7,14 +7,26 @@
 namespace gegege::otsukimi {
 
 struct Otsukimi {
-    lua::LuaEngine luaEngine;
+    lua::LuaEngine mLuaEngine;
+    SDL_Window* mSdlWindow;
+    bool mStopRendering;
 
     void startup()
     {
-        luaEngine.startup();
-        luaEngine.openlibs();
+        mLuaEngine.startup();
+        mLuaEngine.openlibs();
 
-        SDL_Init(SDL_INIT_VIDEO);
+        if (!SDL_Init(SDL_INIT_VIDEO))
+        {
+            SDL_Log("Otsukimi: Couldn't initialize SDL: %s", SDL_GetError());
+        }
+
+        mSdlWindow = SDL_CreateWindow("gegege::Otsukimi", 640, 480, SDL_WINDOW_OPENGL);
+        if (!mSdlWindow)
+        {
+            SDL_Log("Otsukimi: Couldn't create window: %s", SDL_GetError());
+            abort();
+        }
 
         std::filesystem::path path = SDL_GetBasePath();
         SDL_Log("Base Path: %s", path.generic_string().c_str());
@@ -22,7 +34,7 @@ struct Otsukimi {
         // set data/?.lua
         path.append("data");
         path.append("?.lua");
-        auto pkgPath = gegege::lua::getLuaValueString(luaEngine.getTable("package", "path"));
+        auto pkgPath = gegege::lua::getLuaValueString(mLuaEngine.getTable("package", "path"));
         pkgPath.append(";");
         pkgPath.append(path.generic_string());
 
@@ -34,21 +46,56 @@ struct Otsukimi {
         pkgPath.append(";");
         pkgPath.append(path.generic_string());
 
-        luaEngine.setTable("package", "path", gegege::lua::LuaString::make(pkgPath));
+        mLuaEngine.setTable("package", "path", gegege::lua::LuaString::make(pkgPath));
 
         path = SDL_GetBasePath();
         path.append("data");
         path.append("main.lua");
 
-        luaEngine.executeFile(path.generic_string().c_str());
+        mLuaEngine.executeFile(path.generic_string().c_str());
     }
 
     void shutdown()
     {
         SDL_Quit();
 
-        luaEngine.shutdown();
+        mLuaEngine.shutdown();
+    }
+
+    void run()
+    {
+        SDL_Event e;
+        bool bQuit = false;
+
+        while (!bQuit)
+        {
+            while (SDL_PollEvent(&e) != 0)
+            {
+                if (e.type == SDL_EVENT_QUIT)
+                {
+                    SDL_Log("Otsukimi: SDL_EVENT_QUIT is occured");
+                    bQuit = true;
+                }
+
+                if (e.type == SDL_EVENT_WINDOW_MINIMIZED)
+                {
+                    SDL_Log("Otsukimi: SDL_EVENT_WINDOW_MINIMIZED is occured");
+                    mStopRendering = true;
+                }
+                if (e.type == SDL_EVENT_WINDOW_RESTORED)
+                {
+                    SDL_Log("Otsukimi: SDL_EVENT_WINDOW_RESTORED is occured");
+                    mStopRendering = false;
+                }
+            }
+
+            if (mStopRendering)
+            {
+                SDL_Delay(100);
+                continue;
+            }
+        }
     }
 };
 
-}
+} // namespace gegege::otsukimi
--- src/gegege/otsukimi/main.cpp
+++ src/gegege/otsukimi/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char** argv)
 {
     gegege::otsukimi::Otsukimi otsukimi;
     otsukimi.startup();
+    otsukimi.run();
     otsukimi.shutdown();
     return 0;
 }
